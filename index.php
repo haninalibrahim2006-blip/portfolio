e3dcbd1414195a581e70b7ed4d8e2b97e57cffa4
haninalibrahim2006-blip <hanin.alibrahim.2006@gmail.com>
2026-04-09 12:25:05 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,31 +1,85 @@
-<?php
-include("inc/functions.php");
-?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="author" content="Benjamin Porobic">
-    <title>Home</title>
-    <link rel="stylesheet" href="css/style.css">
+  <meta charset="UTF-8">
+  <title>Calculator++</title>
+  <link rel="stylesheet" href="style.css"> 
 </head>
 <body>
 
-<header class="topbar">
-    <!-- Logo -->
-    <a class="logo" href="index.php">Radio Gaga</a>
+<div class="container">
+  <div class="top-part">Calculator++</div>
 
-    <!-- Menu uit database -->
-    <?php displayNavigation(); ?>
-</header>
+  <div class="bottom-part">
+    <form method="post">
+      <input type="text" name="num1" placeholder="Nummer 1" required>
+      <input type="text" name="num2" placeholder="Nummer 2" required>
 
-<main class="home">
-    <h1>Home</h1>
-    <h2>Welkom 🎧</h2>
-    <p>Op deze website kan je albums kiezen en nummers afspelen.</p>
+      <div class="operations">
+        <label><input type="checkbox" name="ops[]" value="add">+</label>
+        <label><input type="checkbox" name="ops[]" value="sub">−</label>
+        <label><input type="checkbox" name="ops[]" value="mul">×</label>
+        <label><input type="checkbox" name="ops[]" value="div">÷</label>
+        <label><input type="checkbox" name="ops[]" value="pow">^</label>
+        <label><input type="checkbox" name="ops[]" value="sqrt">√</label>
+      </div>
 
-    <a class="btn" href="playlist.php">Ga naar Playlist</a>
-</main>
+      <button type="submit" class="btn-calc">=</button>
+      <a href="index.php" class="btn-reset">C</a>
+    </form>
+
+    <?php
+    // --- PHP-berekeningen ---
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+      // Waarden ophalen uit formulier
+      $n1 = $_POST['num1'];
+      $n2 = $_POST['num2'];
+      $ops = $_POST['ops'] ?? [];
+
+      // Controleren of de invoer numeriek is
+      if (is_numeric($n1) && is_numeric($n2)) {
+
+        echo "<div class='result-box'>";
+
+        foreach ($ops as $o) {
+          switch ($o) {
+            case "add":
+              echo "$n1 + $n2 = " . ($n1 + $n2) . "<br>";
+              break;
+
+            case "sub":
+              echo "$n1 - $n2 = " . ($n1 - $n2) . "<br>";
+              break;
+
+            case "mul":
+              echo "$n1 × $n2 = " . ($n1 * $n2) . "<br>";
+              break;
+
+            case "div":
+              echo $n2 != 0
+                ? "$n1 ÷ $n2 = " . ($n1 / $n2) . "<br>"
+                : "Delen door 0!<br>";
+              break;
+
+            case "pow":
+              echo "$n1 ^ $n2 = " . pow($n1, $n2) . "<br>";
+              break;
+
+            case "sqrt":
+              echo "√$n1 = " . sqrt($n1) . "<br>";
+              break;
+          }
+        }
+
+        echo "</div>";
+      } else {
+        echo "<div class='result-box'>Voer geldige nummers in!</div>";
+      }
+    }
+    ?>
+  </div>
+</div>
 
 </body>
 </html>
